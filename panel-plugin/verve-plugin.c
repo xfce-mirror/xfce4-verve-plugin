fb5de70cd76491cee99a246fd156b96457737686
Jannis Pohlmann <jannis@xfce.org>
2006-02-14 22:14:19 +0000

    2006-02-14	Jannis Pohlmann <jannis@xfce.org>
    	* panel-plugin/verve-plugin.c: Added config.h include. This 
    	  fixes translation problems (translations weren't displayed 
    	  before).
    
    
    (Old svn revision: 1057)

--- panel-plugin/verve-plugin.c
+++ panel-plugin/verve-plugin.c
@@ -22,6 +22,10 @@
  *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
+#ifdef HAVE_CONFIG_H
+#include "../config.h"
+#endif 
+
 #include <glib.h>
 #include <X11/Xlib.h>
 #include <X11/Xatom.h>
