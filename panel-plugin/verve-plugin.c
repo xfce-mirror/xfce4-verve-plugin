1ab9414c21f7f6e698f933646eaf13722b23c8de
Olaf Hering <olaf@aepfle.de>
2020-12-19 18:36:29 +0100

    Fix GtkColorChooser usage
    verve-plugin.c:914:31: error: passing argument 1 of 'gtk_color_chooser_get_rgba' from incompatible pointer type [-Werror=incompatible-pointer-types]
    
    Signed-off-by: Olaf Hering <olaf@aepfle.de>

--- panel-plugin/verve-plugin.c
+++ panel-plugin/verve-plugin.c
@@ -911,7 +911,7 @@ verve_plugin_fg_color_changed (GtkColorButton *color_button,
 
   /* Get the entered color */
   GdkRGBA color;
-  gtk_color_chooser_get_rgba (color_button, &color);
+  gtk_color_chooser_get_rgba (GTK_COLOR_CHOOSER(color_button), &color);
   const gchar *color_str = gdk_rgba_to_string (&color);
 
   verve_plugin_update_colors (NULL, color_str, NULL, NULL, verve);
